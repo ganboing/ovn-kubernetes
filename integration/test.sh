d947bd6bf8a64105663808e2dba8146cac93be00
Bo Gan <ganb@vmware.com>
2018-02-01 00:37:12 +0000

    progress

--- integration/test.sh
+++ integration/test.sh
@@ -4,5 +4,5 @@ docker run --rm -it --entrypoint /bin/bash -e TERM=linux -w /root \
   -v /opt/ovn-kubernetes:/opt/ovn-kubernetes \
   -v /opt/cni/bin:/host/opt/cni/bin \
   -v /etc/cni/net.d:/host/etc/cni/net.d \
-  -v "$(readlink -f "$(dirname "$0")")"/host-daemon:/root \
-  ovnkube-host-daemon -l
+  -v "$(readlink -f "$(dirname "$0")")"/docker:/root \
+  ganboing/ovnkube:latest -l
