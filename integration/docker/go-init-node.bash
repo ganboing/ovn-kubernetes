73ba3ec5017798a11230e1008852fad8a667999e
Bo Gan <ganb@vmware.com>
2018-02-01 00:39:40 +0000

    progress

--- integration/docker/go-init-node.bash
+++ integration/docker/go-init-node.bash
@@ -6,7 +6,8 @@ source "$(dirname "${BASH_SOURCE[0]}")/common-api.bash"
 
 if [ "$(get_node_role)" == "worker" ]; then
   /opt/ovn-go-kube/ovnkube -init-node "$(get_node_name)" -ca-cert "$(get_ca_cert_path)" -token "$(get_token)" -apiserver "$(get_api_server)" -cluster-subnet "$(get_cluster_cidr)"
-  sleep 1d
+  #sleep forever
+  tail -f /dev/null
 else
   exit 1
 fi
